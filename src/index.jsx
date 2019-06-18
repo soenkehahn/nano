b59c920e8db5edbd708fb35d00d2ffc7e4481d6d
Sönke Hahn <soenkehahn@gmail.com>
2019-06-18 11:16:12 -0400

    allow to switch to dev config

--- src/index.jsx
+++ src/index.jsx
@@ -6,21 +6,25 @@ import { animated } from "./animated";
 import React from "react";
 import dom from "react-dom";
 
-const config: Config = {
-  sceneSize: 500,
-  zoomVelocity: 1.15,
-  stepTimeDelta: 1000 / 60,
-  velocity: 0.03,
-  prices: { factory: 3 },
-  researchVelocity: 0.00001
-};
-
 const slowDown = null;
 
 if (!module.parent) {
   const appElement = document.getElementById("app");
   if (!appElement) {
     throw new Error("cannot find element 'app'");
+  }
+  const config = {
+    sceneSize: 500,
+    zoomVelocity: 1.15,
+    stepTimeDelta: 1000 / 60,
+    velocity: 0.03,
+    prices: { factory: 3 },
+    researchVelocity: 0.00001
+  };
+  const queryParams = new URLSearchParams(window.location.search);
+  if (queryParams.get("dev")) {
+    config.velocity = config.velocity * 10;
+    config.researchVelocity = config.researchVelocity * 20;
   }
   const SceneRender = animated(
     slowDown,
