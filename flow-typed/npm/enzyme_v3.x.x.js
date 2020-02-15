e2f4e9329d0bcf2c52632a03484f9d0f01d0c957
Sönke Hahn <soenkehahn@gmail.com>
2020-02-14 22:39:30 -0500

    update flow types

--- flow-typed/npm/enzyme_v3.x.x.js
+++ flow-typed/npm/enzyme_v3.x.x.js
@@ -1,12 +1,12 @@
-// flow-typed signature: f6bad512110ebc6da85b1ddda297fe3d
-// flow-typed version: f04d291d8b/enzyme_v3.x.x/flow_>=v0.53.x
+// flow-typed signature: 931d5482afcb022bcbddb841fd08d683
+// flow-typed version: 5175c53189/enzyme_v3.x.x/flow_>=v0.104.x
 
 declare module "enzyme" {
   declare type PredicateFunction<T: Wrapper<*>> = (
     wrapper: T,
     index: number
   ) => boolean;
-  declare type UntypedSelector = string | {[key: string]: number|string|boolean};
+  declare type UntypedSelector = string | { [key: string]: number|string|boolean, ... };
   declare type EnzymeSelector = UntypedSelector | React$ElementType;
 
   // CheerioWrapper is a type alias for an actual cheerio instance
@@ -26,7 +26,7 @@ declare module "enzyme" {
     containsMatchingElement(node: React$Node): boolean,
     containsAllMatchingElements(nodes: React$Node): boolean,
     containsAnyMatchingElements(nodes: React$Node): boolean,
-    dive(option?: { context?: Object }): this,
+    dive(option?: { context?: Object, ... }): this,
     exists(selector?: EnzymeSelector): boolean,
     isEmptyRender(): boolean,
     matchesElement(node: React$Node): boolean,
@@ -47,6 +47,7 @@ declare module "enzyme" {
     unmount(): this,
     text(): string,
     html(): string,
+    invoke(propName: string): (...args: $ReadOnlyArray<any>) => mixed,
     get(index: number): React$Node,
     getDOMNode(): HTMLElement | HTMLInputElement,
     at(index: number): this,
@@ -60,8 +61,8 @@ declare module "enzyme" {
     simulate(event: string, ...args: Array<any>): this,
     simulateError(error: Error): this,
     slice(begin?: number, end?: number): this,
-    setState(state: {}, callback?: () => void): this,
-    setProps(props: {}, callback?: () => void): this,
+    setState(state: {...}, callback?: () => void): this,
+    setProps(props: {...}, callback?: () => void): this,
     setContext(context: Object): this,
     instance(): React$ElementRef<RootComponent>,
     update(): this,
@@ -99,37 +100,44 @@ declare module "enzyme" {
       options?: ?Object
     ): ShallowWrapper<T>,
     equals(node: React$Node): boolean,
-    shallow(options?: { context?: Object }): ShallowWrapper<T>,
+    shallow(options?: { context?: Object, ... }): ShallowWrapper<T>,
     getElement(): React$Node,
     getElements(): Array<React$Node>
   }
 
   declare function shallow<T>(
     node: React$Element<T>,
-    options?: { context?: Object, disableLifecycleMethods?: boolean }
+    options?: {
+      context?: Object,
+      disableLifecycleMethods?: boolean,
+      ...
+    }
   ): ShallowWrapper<T>;
   declare function mount<T>(
     node: React$Element<T>,
     options?: {
       context?: Object,
       attachTo?: HTMLElement,
-      childContextTypes?: Object
+      childContextTypes?: Object,
+      ...
     }
   ): ReactWrapper<T>;
-  declare function render<T>(
-    node: React$Element<T>,
-    options?: { context?: Object }
+  declare function render(
+    node: React$Node,
+    options?: { context?: Object, ... }
   ): CheerioWrapper;
 
   declare module.exports: {
     configure(options: {
       Adapter?: any,
-      disableLifecycleMethods?: boolean
+      disableLifecycleMethods?: boolean,
+      ...
     }): void,
     render: typeof render,
     mount: typeof mount,
     shallow: typeof shallow,
     ShallowWrapper: typeof ShallowWrapper,
-    ReactWrapper: typeof ReactWrapper
+    ReactWrapper: typeof ReactWrapper,
+    ...
   };
 }
